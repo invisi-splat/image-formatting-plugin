f0369b4ecbd782633fcfbfd8da71c1187aab40e9
invisi-splat <bowen.zhu@me.com>
2022-04-04 16:07:36 +0100

    Added image formatting and text colour coding

--- main.ts
+++ main.ts
@@ -1,85 +1,116 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
+import { App, Editor, MarkdownView, MarkdownRenderer, Plugin, PluginSettingTab, Setting } from "obsidian";
 
 interface MyPluginSettings {
-	mySetting: string;
+	padding: number;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	padding: 10 // i have no clue why i'm not allowed to put a semicolon at the end of this line
 }
+
+const colourMap = new Map<string, string>([
+	["bl", "black"],
+	["s", "silver"],
+	["gr", "gray"],
+	["w", "white"],
+	["m", "maroon"],
+	["r", "red"],
+	["p", "purple"],
+	["f", "fuchsia"],
+	["g", "green"],
+	["l", "lime"],
+	["o", "olive"],
+	["y", "yellow"],
+	["n", "navy"],
+	["b", "blue"],
+	["t", "teal"],
+	["a", "aqua"],
+]);
+
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+		console.log("Load")
+		this.registerMarkdownPostProcessor((element, context) => {
+			const images = element.querySelectorAll<HTMLElement>("span.internal-embed"); // ????? .image-embed doesn't work at all
+			for (let index = 0; index < images.length; index++) {
+				let image = images.item(index);
+				let settings = image.nextElementSibling;
+				if (settings.nodeName.toLowerCase() === "code") {
+					if (settings.innerHTML.charAt(0) === "/") {
+						settings.innerHTML = settings.innerHTML.replace("/","");
+						image.style.float = "none";
+					} else {
+						this.format(element, context, image, settings.innerHTML);
+						settings.innerHTML = "";
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
 				}
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		this.registerMarkdownPostProcessor((element, context) => {
+			const codes = element.querySelectorAll<HTMLElement>("code");
+			const pattern: RegExp = /(.*):(.*)/
+			for (let index = 0; index < codes.length; index++) {
+				let code = codes.item(index);
+				if (code.innerHTML.charAt(0) === "/") {
+					code.innerHTML = code.innerHTML.replace("/","");
+				} else {
+					if (pattern.exec(code.innerHTML)) {
+						const wordColour: string = pattern.exec(code.innerHTML)[1];
+						const content: string = pattern.exec(code.innerHTML)[2];
+						this.colour(element, context, wordColour, content, code);
+					}
+				}
+			}
 		});
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new SettingTab(this.app, this));
+	}
+
+	format(element: Element, context: any, image: HTMLElement, settings: string) {
+		let s = settings.valueOf();
+		switch (s.charAt(0).toLowerCase()) {
+			case "r":
+				s = "right";
+				image.style.float = s;
+				image.style.padding = this.settings.padding.toString() + "px";
+				break;
+			case "l":
+				s = "left";
+				image.style.float = s;
+				image.style.padding = this.settings.padding.toString() + "px";
+				break;
+			case "c":
+				image.style.display = "table";
+				image.style.margin = "0 auto";
+			default:
+				image.style.display = "default";
+				image.style.margin = "default";
+				image.style.float = "none";
+				break;
+		}
+	}
+
+	colour(element: Element, context: any, wordColour: string, content: string, code: HTMLElement) {
+		let colourSpan = element.createEl("span");
+		colourSpan.innerHTML = content;
+		if (colourMap.get(wordColour)) {
+			colourSpan.style.color = colourMap.get(wordColour);
+		} else {
+			colourSpan.style.color = wordColour;
+		}
+		console.log(colourSpan)
+		console.log(code)
+		code.parentElement.insertBefore(colourSpan, code);
+		code.innerHTML = "";
 	}
 
 	onunload() {
-
+		//
 	}
 
 	async loadSettings() {
@@ -91,23 +122,7 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
+class SettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
 
 	constructor(app: App, plugin: MyPlugin) {
@@ -119,18 +134,16 @@ class SampleSettingTab extends PluginSettingTab {
 		const {containerEl} = this;
 
 		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Image Formatting Plugin Settings'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Padding')
+			.setDesc('Specify default padding of images in px')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('10')
+				.setValue(this.plugin.settings.padding.toString())
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.padding = parseFloat(value);
 					await this.plugin.saveSettings();
 				}));
 	}
